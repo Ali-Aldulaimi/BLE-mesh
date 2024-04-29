b61dcc93bd92702297ca08eda4b26b66976291f2
Al-dulaimi Ali <alialdulaimi208@gmail.com>
2024-04-29 16:27:07 +0300

    recieve onoff status from broadcaster node

--- src/main.c
+++ src/main.c
@@ -1,50 +1,42 @@
-/* main.c - Application main entry point */
-
-/*
- * Copyright (c) 2017 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/sys/printk.h>
-
 #include <zephyr/settings/settings.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
+//#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/hwinfo.h>
 #include <zephyr/sys/byteorder.h>
-
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/mesh.h>
 #include <zephyr/bluetooth/mesh/access.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <limits.h>
-
-
-
 #include "board.h"
-// Include logging for easier debugging of mesh network issues.
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(mesh_observer, LOG_LEVEL_DBG);
+
+//LOG_MODULE_REGISTER(mesh_observer, LOG_LEVEL_DBG);
 
 
-// Definitions for mesh message operations
+// Definitions of macro mesh operations
 #define OP_ONOFF_GET       BT_MESH_MODEL_OP_2(0x82, 0x01)
 #define OP_ONOFF_SET       BT_MESH_MODEL_OP_2(0x82, 0x02)
 #define OP_ONOFF_SET_UNACK BT_MESH_MODEL_OP_2(0x82, 0x03)
 #define OP_ONOFF_STATUS    BT_MESH_MODEL_OP_2(0x82, 0x04)
 #define OP_SEQ_NUMBER      BT_MESH_MODEL_OP_2(0x82, 0x05)  // Operation code for sequence number
-// In other C files where onoff_state is used
-bool onoff_state = false; // Default state, adjust as necessary
 
+bool onoff_state = false; 
 
+static struct {
+	bool val;
+	uint8_t tid;
+	uint16_t src;
+	uint32_t transition_time;
+	struct k_work_delayable work;
+} onoff;
 
-static uint8_t dev_uuid[16];  // Example: Initialize with specific values if needed
+static uint8_t dev_uuid[16];  // array to store device uuid
 
-// Forward declaration of callback functions
 // Handler for receiving messages with sequence numbers
 static int message_received(const struct bt_mesh_model *model,
                              struct bt_mesh_msg_ctx *ctx,
@@ -79,62 +71,151 @@ static int message_received(const struct bt_mesh_model *model,
     //float plr = ((float)total_missed_packets / (float)(total_received_packets + total_missed_packets)) * 100.0;
     uint32_t plr = (total_missed_packets * 10000) / (total_received_packets + total_missed_packets);
 
-    // Log the received sequence number and PLR using printk
+  
     printk("Total missed packets: %u\n", total_missed_packets);
     printk("Total received packets: %u\n", total_received_packets);
     printk("Received seq_num %u, PLR: %u.%02u%%\n", seq_num, plr / 100, plr % 100);
 
-    return 0; // Return success
+    return 0; 
+}
+static const uint32_t time_res[] = {
+	100,
+	MSEC_PER_SEC,
+	10 * MSEC_PER_SEC,
+	10 * 60 * MSEC_PER_SEC,
+};
+static inline int32_t model_time_decode(uint8_t val)
+{
+	uint8_t resolution = (val >> 6) & BIT_MASK(2);
+	uint8_t steps = val & BIT_MASK(6);
+
+	if (steps == 0x3f) {
+		return SYS_FOREVER_MS;
+	}
+
+	return steps * time_res[resolution];
 }
 
-static int gen_onoff_get(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf);
-static int gen_onoff_set(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf);
-static int gen_onoff_set_unack(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf);
+static inline uint8_t model_time_encode(int32_t ms)
+{
+	if (ms == SYS_FOREVER_MS) {
+		return 0x3f;
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(time_res); i++) {
+		if (ms >= BIT_MASK(6) * time_res[i]) {
+			continue;
+		}
+
+		uint8_t steps = DIV_ROUND_UP(ms, time_res[i]);
+
+		return steps | (i << 6);
+	}
+
+	return 0x3f;
+}
+
+static int gen_onoff_set_unack(const struct bt_mesh_model *model,
+			       struct bt_mesh_msg_ctx *ctx,
+			       struct net_buf_simple *buf)
+{
+	uint8_t val = net_buf_simple_pull_u8(buf);
+	uint8_t tid = net_buf_simple_pull_u8(buf);
+	int32_t trans = 0;
+	int32_t delay = 0;
+
+	if (buf->len) {
+		trans = model_time_decode(net_buf_simple_pull_u8(buf));
+		delay = net_buf_simple_pull_u8(buf) * 5;
+	}
+
+	/* Only perform change if the message wasn't a duplicate and the
+	 * value is different.
+	 */
+	if (tid == onoff.tid && ctx->addr == onoff.src) {
+		/* Duplicate */
+		return 0;
+	}
+
+	if (val == onoff.val) {
+		/* No change */
+		return 0;
+	}
+
+	//printk("set: %s delay: %d ms time: %d ms\n", onoff_str[val], delay,
+	//       trans);
+
+	onoff.tid = tid;
+	onoff.src = ctx->addr;
+	onoff.val = val;
+	onoff.transition_time = trans;
+
+	/* Schedule the next action to happen on the delay, and keep
+	 * transition time stored, so it can be applied in the timeout.
+	 */
+	k_work_reschedule(&onoff.work, K_MSEC(delay));
+
+	return 0;
+}
 
 // Definition of the OnOff Client Model Operations
 static const struct bt_mesh_model_op gen_onoff_cli_op[] = {
     {OP_ONOFF_STATUS, BT_MESH_LEN_MIN(1), message_received},
-    {OP_SEQ_NUMBER, BT_MESH_LEN_EXACT(4), message_received},  // Assuming the seq_num is exactly 4 bytes long
+    {OP_SEQ_NUMBER, BT_MESH_LEN_EXACT(4), message_received},  
     BT_MESH_MODEL_OP_END,
 };
 
-static int onoff_status_send(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, bool onoff_state) {
-    BT_MESH_MODEL_BUF_DEFINE(msg, OP_ONOFF_STATUS, 2);
-    bt_mesh_model_msg_init(&msg, OP_ONOFF_STATUS);
-    net_buf_simple_add_u8(&msg, onoff_state);
+static int onoff_status_send(const struct bt_mesh_model *model,
+			     struct bt_mesh_msg_ctx *ctx)
+{
+	uint32_t remaining;
 
-    return bt_mesh_model_send(model, ctx, &msg, NULL, NULL);
+	BT_MESH_MODEL_BUF_DEFINE(buf, OP_ONOFF_STATUS, 3);
+	bt_mesh_model_msg_init(&buf, OP_ONOFF_STATUS);
+
+	remaining = k_ticks_to_ms_floor32(
+			    k_work_delayable_remaining_get(&onoff.work)) +
+		    onoff.transition_time;
+
+	/* Check using remaining time instead of "work pending" to make the
+	 * onoff status send the right value on instant transitions. As the
+	 * work item is executed in a lower priority than the mesh message
+	 * handler, the work will be pending even on instant transitions.
+	 */
+	if (remaining) {
+		net_buf_simple_add_u8(&buf, !onoff.val);
+		net_buf_simple_add_u8(&buf, onoff.val);
+		net_buf_simple_add_u8(&buf, model_time_encode(remaining));
+	} else {
+		net_buf_simple_add_u8(&buf, onoff.val);
+	}
+
+	return bt_mesh_model_send(model, ctx, &buf, NULL, NULL);
 }
 
-static int gen_onoff_get(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf) {
-    // Here we assume `onoff_state` is a global or static variable tracking the OnOff state
-    extern bool onoff_state;
-    return onoff_status_send(model, ctx, onoff_state);
+
+static int gen_onoff_set(const struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	(void)gen_onoff_set_unack(model, ctx, buf);
+	onoff_status_send(model, ctx);
+
+	return 0;
+}
+static int gen_onoff_get(const struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	onoff_status_send(model, ctx);
+	return 0;
 }
 
-static int gen_onoff_set(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf) {
-    bool new_state = net_buf_simple_pull_u8(buf);
-    // Here we assume `onoff_state` is a global or static variable tracking the OnOff state
-    extern bool onoff_state;
-    onoff_state = new_state;
-
-    // Optionally respond with the new state
-    onoff_status_send(model, ctx, onoff_state);
-    return 0;
-}
-
-static int gen_onoff_set_unack(const struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf) {
-    bool new_state = net_buf_simple_pull_u8(buf);
-    // Here we assume `onoff_state` is a global or static variable tracking the OnOff state
-    extern bool onoff_state;
-    onoff_state = new_state;
-    return 0;
-};
 static const struct bt_mesh_model_op gen_onoff_srv_op[] = {
-    { OP_ONOFF_GET,       BT_MESH_LEN_EXACT(0), gen_onoff_get },
-    { OP_ONOFF_SET,       BT_MESH_LEN_MIN(2),   gen_onoff_set },
-    { OP_ONOFF_SET_UNACK, BT_MESH_LEN_MIN(2),   gen_onoff_set_unack },
-    BT_MESH_MODEL_OP_END,
+	{ OP_ONOFF_GET,       BT_MESH_LEN_EXACT(0), gen_onoff_get },
+	{ OP_ONOFF_SET,       BT_MESH_LEN_MIN(2),   gen_onoff_set },
+	{ OP_ONOFF_SET_UNACK, BT_MESH_LEN_MIN(2),   gen_onoff_set_unack },
+	BT_MESH_MODEL_OP_END,
 };
 
 
@@ -156,10 +237,6 @@ static const struct bt_mesh_comp comp = {
     .elem = elements,
     .elem_count = ARRAY_SIZE(elements),
 };
-//Generate UUID Using HWINFO:
-
-
-
 
 // Bluetooth ready callback
 static void bt_ready(int err) {
@@ -171,7 +248,7 @@ static void bt_ready(int err) {
     printk("Bluetooth initialized\n");
      // Generate a unique UUID
    
-
+    //Generate UUID Using HWINFO:   
     static const struct bt_mesh_prov prov = {
         .uuid = dev_uuid,
         // Other provisioning properties here, like output size, output actions, etc.
@@ -200,27 +277,57 @@ static void bt_ready(int err) {
     }
 }
 
+static void onoff_timeout(struct k_work *work)
+{
+	if (onoff.transition_time) {
+		/* Start transition.
+		 *
+		 * The LED should be on as long as the transition is in
+		 * progress, regardless of the target value, according to the
+		 * Bluetooth Mesh Model specification, section 3.1.1.
+		 */
+		board_led_set(true);
+
+		k_work_reschedule(&onoff.work, K_MSEC(onoff.transition_time));
+		onoff.transition_time = 0;
+		return;
+	}
+
+	board_led_set(onoff.val);
+}
 
 
 
+int main(void)
+{
+	static struct k_work button_work;
+	int err = -1;
 
-int main(void) {
-    
-    int err;
+	printk("Initializing...\n");
 
-    printk("Initializing...\n");
-    if (IS_ENABLED(CONFIG_HWINFO)) {
-        err = hwinfo_get_device_id(dev_uuid, sizeof(dev_uuid));
-    }
-     if (err < 0) {
-        dev_uuid[0] = 0xdd;
-        dev_uuid[1] = 0xdd;
-    }
+	if (IS_ENABLED(CONFIG_HWINFO)) {
+		err = hwinfo_get_device_id(dev_uuid, sizeof(dev_uuid));
+	}
 
-    err = bt_enable(bt_ready);
-    if (err) {
-        printk("Bluetooth enable failed (err %d)\n", err);
-    }
+	if (err < 0) {
+		dev_uuid[0] = 0xdd;
+		dev_uuid[1] = 0xdd;
+	}
 
-    return 0;
+	//k_work_init(&button_work, button_pressed);
+
+	err = board_init(&button_work);
+	if (err) {
+		printk("Board init failed (err: %d)\n", err);
+		return 0;
+	}
+
+	k_work_init_delayable(&onoff.work, onoff_timeout);
+
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(bt_ready);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+	}
+	return 0;
 }
